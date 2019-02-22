30357fab33abd7a6fb70141ad3d089b26b9d67a8
Andre <andrebrreis@gmail.com>
2019-02-22 15:19:42 +0000

    Refactor communication abstraction
    Use key class instead

--- study/run.py
+++ study/run.py
@@ -7,33 +7,32 @@ from bitstring import BitArray
 from implementations.original import OriginalCascade
 from implementations.template import CascadeTemplate
 from study.statistics import Statistics
+from utils.key import Key
 
 
 def replicate(stats, line):
     line = str(line)
-    correct_key = BitArray(bin=stats.status[line].correct_key)
-    key = BitArray(bin=stats.status[line].initial_key)
+    correct_key = Key(stats.status[line].correct_key)
+    key = Key(stats.status[line].initial_key)
     seed = float(stats.status[line].seed)
-    run = OriginalCascade(CascadeTemplate(correct_key, correct_key, None, None), key, stats, seed)
+    run = OriginalCascade(c_key, key, stats, seed)
     run.run_algorithm()
-    print(run.correct_party.key == run.key)
+    print(run.correct_key == run.key)
 
 
 if __name__ == '__main__':
 
     size = 8
-    c_key = BitArray(bin='1'*pow(2, size))
+    c_key = Key('1'*pow(2, size))
     max_err = math.ceil(pow(2, size)*0.05)
-    key = BitArray(bin='1' * pow(2, size))
+    key = Key('1' * pow(2, size))
     for i in range(0, max_err):
         index = random.randint(0, pow(2, size)-1)
         key.invert(index)
 
     stats = Statistics('testfile.csv')
 
-    # run = OriginalCascade(CascadeTemplate(stats.status['1'].correct_key, stats.status['1'].correct_key, None, None), stats.status['1'].initial_key, stats, stats.status['1'].seed)
-    # run.run_algorithm()
-    run = OriginalCascade(CascadeTemplate(c_key, c_key, None, None), key, stats, time.time())
+    run = OriginalCascade(c_key, key, stats, time.time())
     run.run_algorithm()
 
     stats = Statistics('testfile2.csv', infile='testfile.csv')
