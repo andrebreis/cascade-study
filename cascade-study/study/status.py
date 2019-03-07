940f2238a92f00a7f71b7ec3f1b3d68d5a34baf4
Andre <andrebrreis@gmail.com>
2019-03-07 17:11:05 +0000

    Save each iteration info functionality

--- cascade-study/study/status.py
+++ cascade-study/study/status.py
@@ -1,31 +1,39 @@
 import os
 from math import log2
 
+NO_LOG = 1
+FINAL_DATA = 2
+ALL_DATA = 3
+
 
 class Status(object):
 
-    def __init__(self, filename, dataset_file, dataset_line, seed):
+    def __init__(self, filename, dataset_file, dataset_line, seed, level=NO_LOG):
+        if level > NO_LOG:
+            os.makedirs(os.path.dirname(os.path.abspath(filename)), exist_ok=True)
+            if not os.path.isfile(filename):
+                with open(filename, 'w') as f:
+                    f.write('dataset file,dataset line,channel error rate,real error rate,' +
+                            'correct,ber,efficiency,channel uses,exchanged msg length,seed,[cor,ber,eff,cu,eml]*\n')
+            self.filename = filename
 
-        os.makedirs(os.path.dirname(os.path.abspath(filename)), exist_ok=True)
-        if not os.path.isfile(filename):
-            with open(filename, 'w') as f:
-                f.write('dataset file,dataset line,channel error rate,real error rate,' +
-                        'correct,ber,efficiency,channel uses,exchanged msg length,seed\n')
-        self.filename = filename
+            self.dataset_file = dataset_file
+            self.dataset_line = dataset_line
+            self.seed = seed
 
-        self.dataset_file = dataset_file
-        self.dataset_line = dataset_line
-        self.seed = seed
+            self.correct_key = None
+            self.channel_error_rate = 0.0
+            self.real_error_rate = 0.0
 
-        self.correct_key = None
-        self.channel_error_rate = 0.0
-        self.real_error_rate = 0.0
+            self.final_key = None
+            self.channel_uses = []
+            self.correct = True
+            self.bit_error_ratio = 0
+            self.efficiency = 1.0
 
-        self.final_key = None
-        self.channel_uses = []
-        self.correct = True
-        self.bit_error_ratio = 0
-        self.efficiency = 1.0
+            self.iterations_data = ''
+
+            self.level = level
 
     def initialize_run(self, c_key, channel_error, real_error):
         self.correct_key = c_key
@@ -33,13 +41,23 @@ class Status(object):
         self.real_error_rate = real_error
 
     def start_iteration(self, channel_use):
-        self.channel_uses.append([channel_use['len']])
+        if self.level > NO_LOG:
+            self.channel_uses.append([channel_use['len']])
 
     def start_block(self):
-        self.channel_uses[-1].append([])
+        if self.level > NO_LOG:
+            self.channel_uses[-1].append([])
 
     def add_channel_use(self, channel_use):
-        self.channel_uses[-1][-1].append(channel_use['len'])
+        if self.level > NO_LOG:
+            self.channel_uses[-1][-1].append(channel_use['len'])
+
+    def save_iteration_info(self, key):
+        if self.level == ALL_DATA:
+            self.final_key = key
+            self.calculate_parameters()
+            self.iterations_data += '[%s,%s,%s,%s,%s]' % (
+                self.correct, self.bit_error_ratio, self.efficiency, self.num_channel_uses(), self.exchanged_msg_len())
 
     def _calculate_ber(self):
         num_errors = 0
@@ -67,39 +85,44 @@ class Status(object):
         # return (1 - self.exchanged_msg_len() / len(self.correct_key)) / (
         #         -self.error_rate * log2(self.error_rate) - (1 - self.error_rate) * log2(1 - self.error_rate))
         return self.exchanged_msg_len() / (self.correct_key.length * (
-              -self.channel_error_rate * log2(self.channel_error_rate) - (1 - self.channel_error_rate) * log2(
-               1 - self.channel_error_rate)))
+                -self.channel_error_rate * log2(self.channel_error_rate) - (1 - self.channel_error_rate) * log2(
+            1 - self.channel_error_rate)))
 
     def end_run(self, final_key):
-        self.final_key = final_key
-        self.calculate_parameters()
-        self.flush_to_file()
+        if self.level > NO_LOG:
+            self.final_key = final_key
+            self.calculate_parameters()
+            self.flush_to_file()
 
     def calculate_parameters(self):
         if self.final_key != self.correct_key:
             self.correct = False
             self.bit_error_ratio = self._calculate_ber()
+        else:
+            self.correct = True
+            self.bit_error_ratio = 0
 
         self.efficiency = self._calculate_efficiency()
 
     def flush_to_file(self):
         with open(self.filename, 'a') as f:
-            f.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (self.dataset_file,
-                                                         self.dataset_line,
-                                                         self.channel_error_rate,
-                                                         self.real_error_rate,
-                                                         self.correct,
-                                                         self.bit_error_ratio,
-                                                         self.efficiency,
-                                                         self.num_channel_uses(),
-                                                         self.exchanged_msg_len(),
-                                                         self.seed))
+            f.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (self.dataset_file,
+                                                            self.dataset_line,
+                                                            self.channel_error_rate,
+                                                            self.real_error_rate,
+                                                            self.correct,
+                                                            self.bit_error_ratio,
+                                                            self.efficiency,
+                                                            self.num_channel_uses(),
+                                                            self.exchanged_msg_len(),
+                                                            self.seed,
+                                                            self.iterations_data))
 
     @staticmethod
-    def from_line(output_filename, input_filename, line_number):
+    def from_line(output_filename, input_filename, line_number, level):
         with open(input_filename, 'r') as f:
             for i, line in enumerate(f):
                 if i == line_number:
                     split = line.split(',')
-                    status = Status(output_filename, split[0], int(split[1]), split[-1].replace('\n', ''))
+                    status = Status(output_filename, split[0], int(split[1]), split[-1].replace('\n', ''), level)
                     return status
