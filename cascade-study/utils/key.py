4bc79d5981f301ec3a0801e0609a7ce317cece8d
Andre <andrebrreis@gmail.com>
2019-03-22 15:05:58 +0000

    Improve bitstring access efficiency

--- cascade-study/utils/key.py
+++ cascade-study/utils/key.py
@@ -11,8 +11,9 @@ class Key(BitArray):
 
     def calculate_block_parity(self, block_indexes):
         parity = 0
+        bitstring = self.bin
         for index in block_indexes:
-            parity = (parity + int(self.bin[index])) % 2
+            parity = (parity + int(bitstring[index])) % 2
         return parity
 
     def calculate_parities(self, block_list):
@@ -23,7 +24,9 @@ class Key(BitArray):
 
     def hamming_distance(self, other_key):
         num_errors = 0
-        for i in range(0, len(self.bin)):
-            if self.bin[i] != other_key.bin[i]:
+        self_bitstring = self.bin
+        other_bitstring = other_key.bin
+        for i in range(0, len(self_bitstring)):
+            if self_bitstring[i] != other_bitstring[i]:
                 num_errors += 1
         return num_errors
